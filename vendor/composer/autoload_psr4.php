7680921b1f95c86bbe46a371432da8f037f56cac
Joseph Earl Weiner <JWeiner@madisoncollege.edu>
2018-06-26 05:41:26 -0500

    commerce stripe paypal and chaos tools downloaded and enabled

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -45,6 +45,7 @@ return array(
     'Symfony\\Component\\BrowserKit\\' => array($vendorDir . '/symfony/browser-kit'),
     'Symfony\\Cmf\\Component\\Routing\\' => array($vendorDir . '/symfony-cmf/routing'),
     'Symfony\\Bridge\\PsrHttpMessage\\' => array($vendorDir . '/symfony/psr-http-message-bridge'),
+    'Stripe\\' => array($vendorDir . '/stripe/stripe-php/lib'),
     'Stecman\\Component\\Symfony\\Console\\BashCompletion\\' => array($vendorDir . '/stecman/symfony-console-completion/src'),
     'Robo\\' => array($vendorDir . '/consolidation/robo/src'),
     'Psy\\' => array($vendorDir . '/psy/psysh/src'),
